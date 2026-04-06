61b96660b137187f6024b285574c703db8ccd45b
Vinc <vincenzogulletta@gmail.com>
2026-04-06 02:57:25 +0200

    Test webhook deploy (build 11)

--- config.php
+++ config.php
@@ -13,7 +13,7 @@ define('DEFAULT_TTL', 300);
 
 // Versione applicazione
 define('APP_VERSION', '1.0');
-define('APP_BUILD', 10);
+define('APP_BUILD', 11);
 
 // Integrazione Plesk DNS API
 // Credenziali nel file .env (non versionato)
